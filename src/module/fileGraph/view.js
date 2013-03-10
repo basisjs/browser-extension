d4045b5b4dbde9c9ca02a69c839389d86c9c56f2
Roman Dvornov <rdvornov@gmail.com>
2013-03-11 03:07:26 +0400

    control panel with showing error messages and refresh button

--- src/module/fileGraph/view.js
+++ src/module/fileGraph/view.js
@@ -1,10 +1,52 @@
+basis.require('basis.ui.button');
 
-var view = basis.resource('lib/file-graph-viewer/src/module/view/index.js').fetch();
+var graphView = basis.resource('lib/file-graph-viewer/src/module/view/index.js').fetch();
+var controlPanel = new basis.ui.Node({
+  template: resource('template/panel.tmpl'),
+  binding: {
+    refreshButton: new basis.ui.button.Button({
+      autoDelegate: true,
+      caption: 'Refresh',
+      click: getFileGraph,
+      handler: {
+        stateChanged: function(){
+          if (this.state == basis.data.STATE.PROCESSING)
+            this.disable();
+          else
+            this.enable();
+        }
+      }
+    }),
+    error: {
+      events: 'stateChanged',
+      getter: function(node){
+        return node.state.data;
+      }
+    }
+  }
+});
+
+function getFileGraph(){
+  app.transport.call('getFileGraph');
+  controlPanel.setState(basis.data.STATE.PROCESSING);
+}
 
 app.transport.onMessage('fileGraph', function(data){
-  if (!data.err && data.data)
-    view.dataType.loadMap(data.data.toObject());
+  if (data && !data.err && data.data)
+  {
+    controlPanel.setState(basis.data.STATE.READY);
+    graphView.dataType.loadMap(data.data.toObject());
+  }
+  else
+    controlPanel.setState(basis.data.STATE.ERROR, (data && data.err) || 'Wrong data from server');
 });
-app.transport.call('getFileGraph');
 
-module.exports = view;
+getFileGraph();
+
+module.exports = new basis.ui.Node({
+  template: resource('template/view.tmpl'),
+  binding: {
+    graph: graphView,
+    panel: controlPanel
+  }
+});
