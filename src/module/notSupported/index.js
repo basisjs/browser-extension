c50fd1a49fc0b3b4c225af680ec92ebd31cf20a0
Roman Dvornov <rdvornov@gmail.com>
2013-03-16 21:15:46 +0400

    small fixes

--- src/module/notSupported/index.js
+++ src/module/notSupported/index.js
@@ -1,11 +1,13 @@
 basis.require('basis.dom');
 basis.require('basis.ui');
 
-module.exports = new basis.ui.Node({
+var view = new basis.ui.Node({
   template: resource('template/view.tmpl'),
   container: document.body
 });
 
 app.transport.ready(basis.fn.runOnce(function(){
-  basis.dom.remove(module.exports.element);
+  basis.dom.remove(view.element);
 }));
+
+module.exports = view;
