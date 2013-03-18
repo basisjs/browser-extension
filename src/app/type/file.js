a12efbd20c8f3fd2fb008e46a8de7b3ee207e801
Roman Dvornov <rdvornov@gmail.com>
2013-03-18 19:43:05 +0400

    simplify File type

--- src/app/type/file.js
+++ src/app/type/file.js
@@ -1,114 +1,76 @@
+basis.require('basis.data');
+basis.require('basis.entity');
+basis.require('app.transport');
 
-  basis.require('basis.data');
-  basis.require('basis.entity');
-  basis.require('basis.data.dataset');
+var nsEntity = basis.entity;
+var STATE = basis.data.STATE;
 
-  basis.require('app.transport');
 
-  var nsEntity = basis.entity;
-  var nsDataset = basis.data.dataset;
+//
+// define type
+//  
+var File = new nsEntity.EntityType({
+  name: 'File',
+  fields: {
+    filename: nsEntity.StringId,
+    type: String,
+    content: function(value){ 
+      return value == null ? null : String(value);
+    },
+    declaration: basis.fn.$self,
+    resources: basis.fn.$self
+  }
+});
 
-  var STATE = basis.data.STATE;
-  
-  var File = new nsEntity.EntityType({
-    name: 'File',
-    fields: {
-      filename: nsEntity.StringId,
-      type: String,
-      content: function(value){ 
-        return value == null ? null : String(value);
-      },
-      declaration: basis.fn.$self,
-      resources: basis.fn.$self
-    }
-  });
 
-  File.createFile = function(filename){
-    app.transport.invoke('createFile', filename);
-  };
+//
+// extensions
+//
+File.createFile = function(filename){
+  app.transport.invoke('createFile', filename);
+};
 
-  var FileClass = File.entityType.entityClass;
-
-  FileClass.extend({
-    read: function(){
+File.entityType.entityClass.extend({
+  state: STATE.UNDEFINED,
+  syncAction: function(){
+    this.read();
+  },
+  read: function(){
+    this.setState(STATE.PROCESSING);
+    app.transport.invoke('readFile', this.data.filename);
+  },
+  save: function(){
+    if (this.modified)
+    {
       this.setState(STATE.PROCESSING);
-      app.transport.invoke('readFile', this.data.filename);
-    },
-    save: function(){
-      if (this.modified)
-      {
-        this.setState(STATE.PROCESSING);
-        app.transport.invoke('saveFile', this.data.filename, this.data.content);
-      }
+      app.transport.invoke('saveFile', this.data.filename, this.data.content);
     }
-  });
-
-  var fileHandler = function(){
-    if (this.subscriberCount > 0 && (this.state == STATE.UNDEFINED || this.state == STATE.DEPRECATED))
-      this.read();
-  };
-  FileClass.extend({
-    state: STATE.UNDEFINED,
-    event_subscribersChanged: function(){
-      FileClass.superClass_.prototype.event_subscribersChanged.call(this);
-      fileHandler.call(this);
-    },
-    event_stateChanged: function(oldState){
-      FileClass.superClass_.prototype.event_stateChanged.call(this, oldState);
-      fileHandler.call(this);
-    }
-  });
-
-  app.transport.onMessage('filesChanged', function(data){
-    var f;
-    if (data.inserted)
-      for (var i = 0, file; file = data.inserted[i]; i++)
-      {
-        if (file.content == "null")
-          delete file.content;
-
-        File(file);
-      }
-
-    if (data.deleted)
-      for (var i = 0, filename; filename = data.deleted[i]; i++)
-        File(filename).destroy();
-  });
-
-  app.transport.onMessage('updateFile', function(data){
-    var file = File(data.filename);
-
-    file.commit(data);
-
-    file.setState(data.content == null ? STATE.UNDEFINED : STATE.READY);
-  });
+  }
+});
 
 
-  //
-  // Datasets
-  //
-  File.FilesByFolder = new nsDataset.Split({
-    source: File.all,
-    rule: function(object){
-      var path = object.data.filename.split("/");
-      path.pop();
-      return path.join('/');
-    }
-  });
+//
+// transport binding
+//
+app.transport.onMessage('filesChanged', function(data){
+  if (data.inserted)
+    for (var i = 0, file; file = data.inserted[i]; i++)
+      File(file);
 
-  var files = new nsDataset.Subset({
-    source: File.all,
-    rule: function(object){
-      return object.data.type == 'file';
-    }
-  });
+  if (data.deleted)
+    for (var i = 0, filename; filename = data.deleted[i]; i++)
+      File(filename).destroy();
+});
 
-  File.FilesByType = new nsDataset.Split({
-    source: files,
-    rule: function(object){
-      return object.data.filename.split('.').pop();
-    }
-  });
+app.transport.onMessage('updateFile', function(data){
+  var file = File(data.filename);
+
+  file.update(data);
+  file.setState(data.content == null ? STATE.UNDEFINED : STATE.READY);
+});
 
 
-  module.exports = File;
+//
+// export
+//
+module.exports = File;
