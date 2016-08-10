d8a2304aef2e63d65a5c37eee3c88e57cc8bb36a
Roman Dvornov <rdvornov@gmail.com>
2016-08-10 17:46:43 +0300

    change file layout and clean up

---
+++ index.js
@@ -0,0 +1,5 @@
+chrome.devtools.panels.create(
+  'basis.js (dev)',
+  'img/icon32x32.png',
+  'src/plugin.html'
+);
--- plugin/plugin.js
+++
@@ -1,5 +0,0 @@
-      chrome.devtools.panels.create(
-        "basis.js (dev)",
-        "icon32x32.png",
-        "plugin-content.html"
-      );
